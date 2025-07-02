be3c543a307e4376bdebf3b3c3c5ca29c49cc9a4
SimM20 <83238073+SimM20@users.noreply.github.com>
2025-07-01 22:39:14 -0300

    Made some changes

--- Portfolio/js/calculator.js
+++ Portfolio/js/calculator.js
@@ -1,14 +1,24 @@
 const body = document.getElementById('body');
+let isBad = false;
 
 // Agregar items a la calculadora
 function Add(num) 
 {
     body.style.color = '#1a237e';
-    if (body.innerText === '0' || body.innerText === 'Error') { body.innerText = num; }
-    else { body.innerText += num; }
+    if (body.innerText === '0' || body.innerText === 'Error') body.innerText = num;
+    else 
+    { 
+        if (isBad) // Si el EASTER EGG se activó, reemplazo el texto por el número y luego sigo agregando números normalmente
+        {
+            body.innerText = num;
+            isBad = false;
+        }
+        else body.innerText += num;
+    }
 
     if (body.innerText === '43954723') //EASTER EGG
     { 
+        isBad = true;
         body.innerText = 'QUE HACES FLACO';
         body.style.color = 'red';
     }
